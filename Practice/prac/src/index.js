3aa905b8f56f082e5430acf9dcbdae1451309fab
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-12 03:58:41 +0500

    Basic Event Cleared

--- Practice/prac/src/index.js
+++ Practice/prac/src/index.js
@@ -1,33 +1,21 @@
 import { wait } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//CSS
 import './index.css';
 
-
+//Js
+import {Books} from './books'
+import Book from './Book'
 
 function BookList(){
-  let Blist=[{Title:"Python Crash Course",Image:"https://m.media-amazon.com/images/I/71sL0Qpd+YL._AC_UY218_.jpg",Author:"Eric Matthes"},
-             {Title:"The Self-Taught Programmer",Image:"https://m.media-amazon.com/images/I/51+Z8QVq+WL._AC_UY218_.jpg",Author:"Cory Althoff"}  
-             ]
   return (
    <section className='booklist'>
-    {Blist.map((x)=>{return <Book image={x.Image} title={x.Title} author={x.Author}/>})}
+    {Books.map((x)=>{return <Book key={x.id} book={x}/>})}
     </section>
   );
 }
-
-
-const Book=(props)=>{
-  return (
-  <div className='book'>
-   <img src={props.image} alt='Error'/>
-   <h1>{props.title}</h1>
-   <h4>{props.author}</h4>
-  </div>
-  );
-}
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
