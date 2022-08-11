136f8c3aa1fe9ed97f2b4a2672f0731860afb47b
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-11 05:02:47 +0500

    Practicing Components

--- Practice/prac/src/index.js
+++ Practice/prac/src/index.js
@@ -1,15 +1,43 @@
+import { wait } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 
-function Greeting(){
-  return <h4>Hello, you Fucker HHE</h4>;
+function BookList(){
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
+
+
+const Book=()=>{
+  return (
+  <div className='book'>
+    <Image/>
+    <Title/>
+    <Author/>
+  </div>
+  );
+}
+
+const Image=()=><img src='https://m.media-amazon.com/images/I/71sL0Qpd+YL._AC_UY218_.jpg' alt='Error'/>;
+const Title=()=><h1>Python Crash Course</h1>;
+const Author=()=><h4>Eric Matthes</h4>;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Greeting/>
+    <BookList/>
   </React.StrictMode>
 );
+
+
+
