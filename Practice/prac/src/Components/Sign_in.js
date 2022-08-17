c6237cd68e0ab75c08b4cbf8b91d7fb344410abc
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-17 21:58:24 +0500

    Sign-in page completed

--- Practice/prac/src/Components/Sign_in.js
+++ Practice/prac/src/Components/Sign_in.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import './CSS/Sign_in.css'
 
 function Sign_in() {
   const [Name,setName]=useState("");
@@ -9,26 +10,26 @@ function Sign_in() {
     <div className='Form'>
         <form className='Form-Container' autoComplete=''>
             {/*Name*/}
-            <div>
+            <div className='Name'>
             <label>Full-Name</label>
-            <input type={"text"}  placeholder={"Ente Name"} onChange={(e)=>{setName(e.target.value)}}/><br/>
+            <input type={"text"} className={"input-1"} placeholder={"Ente Name"} onChange={(e)=>{setName(e.target.value)}}/><br/>
             </div>
 
             {/*Password*/}   
-            <div>
+            <div className='Password'>
             <label>Password</label>
-            <input type={"password"} placeholder={"Ente Password"} onChange={(e)=>{setPassword(e.target.value)}}/><br/>
+            <input type={"password"} className={"input-1"} placeholder={"Ente Password"} onChange={(e)=>{setPassword(e.target.value)}}/><br/>
             </div>
 
             {/*Email*/}
-            <div>
+            <div className='Email'>
                <label>Email</label>
-               <input type={"email"} placeholder={"Ente Email"} onChange={(e)=>{setEmail(e.target.value)}}/><br/>
+               <input type={"email"} className={"input-1 em"} placeholder={"Ente Email"} onChange={(e)=>{setEmail(e.target.value)}}/><br/>
             </div>
 
             {/*Button*/}   
             <div className='btn-container'>
-               <button type={"submit"} onClick="">Submit</button>
+               <button type={"submit"} className={'btn'} onClick="">Submit</button>
             </div>
         </form>
     </div>
