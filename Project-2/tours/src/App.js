56c8f1960f7c5f9dc3949f434c4aedb11cb099a1
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-21 03:05:56 +0500

    Project-3: Completed

--- Project-2/tours/src/App.js
+++ Project-2/tours/src/App.js
@@ -5,21 +5,37 @@ import Tour from './Component/Tours';
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  const [Loading, setLoading] = useState(false);
+  const [Loading, setLoading] = useState(true);
   const [Tours, setTours] = useState([]);
 
-  const fetchTours=async()=>{
-    setLoading(true);
-    const response=await fetch(url);
-    const tours=await response.json();
-    setTours(tours);
+  const removeTour=(id)=>{
+    const NewTour=Tours.filter(d=>(id!==d.id));
+    setTours(NewTour);
   }
-  useEffect(()=>fetchTours(),[])
+
+  const fetchTours= async ()=>{
+    setLoading(true);
+    try
+    {
+        const response=await fetch(url);
+        const tours=await response.json();
+        setLoading(false);
+        setTours(tours);
+      }catch(error){
+        console.log(error);
+      }
+  };
+
+useEffect(()=>{fetchTours()},[])
 
   return (
     <main>
       {Loading&&<Load />}
-      {!Loading&&<Tour tour={Tours}/>}
+      {!Loading&&<Tour tour={Tours} removeTour={removeTour}/>}
+      {Tours.length==0&&<div className='title'>
+         <h2>no tours left</h2>
+         <button className='btn'  onClick={fetchTours}>Refresh</button>
+      </div>}
     </main>
   );
 }
--- Project-2/tours/src/Component/Tours.js
+++ Project-2/tours/src/Component/Tours.js
@@ -1,8 +1,17 @@
 import React,{useState} from 'react'
+import Tour from './Tour'
 
-function Tours() {
+function Tours({tour,removeTour}) {
   return (
-    <div>Tours</div>
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+         <div className='underline'></div>
+      </div>
+      <div>
+         {tour.map(data=>(<Tour key={data.id} {...data} removetour={removeTour}></Tour>))}
+      </div>
+    </section>
   )
 }
 
