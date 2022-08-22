68a5e783acada8ba4cfc8521a6a167853fb45e5e
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-22 06:54:09 +0500

    Starting Project-4

---
+++ Project-4/accordion/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Ques  from './Component/Question'
+import data from './Component/data'
+
+function App() {
+  return (
+    <div>Accordian</div>
+  )
+}
+
+export default App
---
+++ Project-4/accordion/src/Component/Question.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  {AiOutlineMinus,AiOutlinePlus} from 'react-icons/fa'
+
+function Question() {
+  return (
+    <div>Question</div>
+  )
+}
+
+export default Question
